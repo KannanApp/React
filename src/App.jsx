ed5559206bb9ad2375a241b0dd6cdede0937484c
Kannan App Developer <106946242+KannanApp@users.noreply.github.com>
2023-08-12 21:56:35 +0530

    implemented custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
-import {useState, useEffect} from "react";
 import './App.css';
 import BlogList from "./Dashboard/BlogList";
 import { TextBox } from "./Components/TextBox";
+import { useFetch } from "./Constants/useFetch";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState('');
-  
+  const [data, isLoading, error, setData] = useFetch('https://jsonplaceholder.typicode.com/posts');  
   const handleDelete = (idx) => {
     const res = data.filter((item)=> item?.id !== idx);
     setData(res);
   }
-
-  useEffect(()=> {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((data) => {
-      if(!data.ok){
-        throw Error("Smething went wrong in URL");
-      }
-      return data.json();
-    })
-    .then((res)=> {
-      setData(res);
-      setIsLoading(false);
-      setError('');
-    })
-    .catch(err=> {
-      setIsLoading(false);
-      setError(err.message);
-    });
-  }, []);
 
   return (
     <div className="App">
---
+++ src/Constants/useFetch.js
@@ -0,0 +1,26 @@
+import {useState, useEffect} from "react";
+export const useFetch = (url) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState('');
+    useEffect(() => {
+        fetch(url)
+            .then((data) => {
+                if (!data.ok) {
+                    throw Error("Smething went wrong in URL");
+                }
+                return data.json();
+            })
+            .then((res) => {
+                setData(res);
+                setIsLoading(false);
+                setError('');
+            })
+            .catch(err => {
+                setIsLoading(false);
+                setError(err.message);
+            });
+    }, []);
+
+    return [data, isLoading, error]
+}
